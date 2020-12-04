64daf6bacf9053fb20a5b018a55b946768ecbe60
RhjHasan <rhjhasan@yahoo.com>
2020-12-04 13:52:18 +0800

    update

--- main/headerscript.js
+++ main/headerscript.js
@@ -1,3 +1,4 @@
+
 const indicator = document.querySelector(".nav-indicator");
 const items = document.querySelectorAll(".nav-item");
 
@@ -6,18 +7,20 @@ function handleIndicator(el) {
     item.classList.remove("is-active");
     item.removeAttribute("style");
   });
-
+  
   indicator.style.width = `${el.offsetWidth}px`;
   indicator.style.left = `${el.offsetLeft}px`;
   indicator.style.backgroundColor = el.getAttribute("active-color");
-
+  el.scrollIntoView({behavior: "smooth" ,inline: "center"});
   el.classList.add("is-active");
+  
   el.style.color = el.getAttribute("active-color");
 }
 
 items.forEach((item, index) => {
   item.addEventListener("click", e => {
     handleIndicator(e.target);
+    
   });
   item.classList.contains("is-active") && handleIndicator(item);
 });
